b7d287120d6f948aa8535bd106b84305b45d85d7
Jur9x <jirka.ryba02@gmail.com>
2022-06-20 20:19:57 +0200

    1.0 Hotovo

---
+++ src/api/stadion/routes/stadion.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * stadion router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::stadion.stadion');
---
+++ src/api/stadion/services/stadion.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * stadion service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::stadion.stadion');
